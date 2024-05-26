da82cef87296f72d438f8227b38e169c5dc44c64
Iury Filho <iuryfmmf@gmail.com>
2024-05-26 16:46:47 -0300

    Relógio adicionado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+	const link = "https://reactjs.org";
+	const [ativado, setAtivado] = useState(props.ativado);
+	const [time, setTime] = useState("");
+
+	const ativa = () => setAtivado(!ativado);
+
+	useEffect(() => {
+		const interval = setInterval(relogio, 100);
+		return () => clearInterval(interval);
+	}, []);
+
+	const relogio = () => {
+		const date = new Date();
+		const currentTime =
+			date.toLocaleTimeString() +
+			"." +
+			Math.floor(date.getMilliseconds() / 100);
+		setTime(currentTime);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href={link}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>{time}</p>
+				<button onClick={ativa}>{ativado ? "Desativar" : "Ativar"}</button>
+				{ativado && <p>Ativado</p>}
+			</header>
+		</div>
+	);
 }
 
 export default App;
